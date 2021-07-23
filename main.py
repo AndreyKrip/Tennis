8ccb493d7b0b945f4181aa42538b70922ecd0892
AndreyKrip <oreal404@mail.ru>
2021-07-23 18:17:08 +0300

    Added the main function and implemented the rendiring of the workspace (screen)

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello world!")
+# Tennis
+#
+# v-0.0.1
+#
+# This project is intended for further analysis of the neural network
+#
+# Author: AndreyKrip
+
+import pygame
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((800, 600))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+
+        backcolor = (10,34,140)
+        screen.fill(backcolor)
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
